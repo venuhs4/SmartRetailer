ea19d1324604a05d5c31a5d3a1b09791d0d1fb00
Venu Prasad H S <venuhs4@gmail.com>
2016-01-10 22:11:53 +0530

    demovable app

--- SmartRetailer2/www/app/app.js
+++ SmartRetailer2/www/app/app.js
@@ -13,18 +13,58 @@
             });
         })
         .config(function ($stateProvider, $urlRouterProvider) {
+
+            var idUserLogedIn = localStorage['idUserLogedIn'];
+            var isDefaultRetailerSet = localStorage['defaultRetailer'];
+
+            if (idUserLogedIn == null) {
+                console.warn("user not loged in");
+                $urlRouterProvider.otherwise("/login");
+            }
+            else if (isDefaultRetailerSet == null) {
+                console.warn("default retailer not set");
+                $urlRouterProvider.otherwise("/home");
+            }
+            else {
+                console.info('everything set');
+                $urlRouterProvider.otherwise("/products");
+            }
             $stateProvider
-            .state("app", {
-                url: "/app",
-                abstract: true,
-                templateUrl: "app/templates/view-menu.html",
-                controller: "appCtrl"
+            .state("register", {
+                url: "/register",
+                templateUrl: "app/templates/view-register.html",
+                controller: "registerCtrl"
             })
-            .state("app.home", {
+            .state("login", {
+                url: "/login",
+                templateUrl: "app/templates/view-login.html",
+                controller: "loginCtrl"
+            })
+            .state("home", {
                 url: "/home",
                 templateUrl: "app/templates/view-home.html",
                 controller: "homeCtrl"
+            }).state("products", {
+                url: "/products",
+                templateUrl: "app/templates/view-products.html",
+                controller: "productsCtrl"
+            }).state("profile", {
+                url: "/profile",
+                templateUrl: "app/templates/view-profile.html",
+                controller: "profileCtrl"
+            }).state("orders", {
+                url: "/orders",
+                templateUrl: "app/templates/view-orders.html",
+                controller: "ordersCtrl"
+            }).state("settings", {
+                url: "/settings",
+                templateUrl: "app/templates/view-settings.html",
+                controller: "settingsCtrl"
+            }).state("feedback", {
+                url: "/feedback",
+                templateUrl: "app/templates/view-feedback.html",
+                controller: "feedbackCtrl"
             });
-            $urlRouterProvider.otherwise("/app/home");
+
         });
 })();
--- SmartRetailer2/www/app/controllers.js
+++ SmartRetailer2/www/app/controllers.js
@@ -4,6 +4,90 @@
     angular.module("myapp.controllers", ['myapp.utils', 'ionic'])
 
     .controller("appCtrl", ["$scope", function ($scope) {
+
+    }])
+
+    .controller("registerCtrl", ["$scope", "$state", function ($scope, $state) {
+        $scope.iderrormessage = '';
+        $scope.user = {
+            id: '',
+            deviceID: ''
+        };
+
+        console.log("registerCtrl");
+        $scope.register = function (form) {
+            console.log(form);
+            $state.go('home');
+        };
+        $scope.validateID = function () {
+            console.log($scope.user.id);
+            if ($scope.user.id != null) {
+                $scope.iderrormessage = ''
+                console.log('not show');
+                return false;
+            }
+            else {
+                $scope.iderrormessage = 'not a valid input';
+                console.log('show');
+                return true;
+            }
+        }
+    }])
+
+    .controller("loginCtrl", ["$scope", "$state", "$customlocalstorage", "$http", function ($scope, $state, $customlocalstorage, $http) {
+        $scope.iderrormessage = '';
+        $scope.user = {
+            id: '',
+            deviceID: ''
+        };
+
+        console.log("loginCtrl");
+        $scope.login = function () {
+            $scope.user.deviceID = device.uuid;
+            var loginSuccess = false;
+
+            var req = {
+                method: 'POST',
+                url: 'http://localhost:36485/api/registation',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: JSON.stringify($scope.user)
+            }
+
+            $http(req).then(function (res) {
+                console.log(res.data);
+                if (res.data.registrationStatus == 'OK') {
+                    loginSuccess = true;
+                    $customlocalstorage.setObject('registration', $scope.user)
+                    $customlocalstorage.set('idUserLogedIn', $scope.user.id);
+                    console.log("CHK:" + $customlocalstorage.get('idUserLogedIn'));
+                }
+                console.info("registration post success");
+            }, function () {
+                console.warn("registration post failed");
+            });
+
+            if (loginSuccess) {
+                $state.go('home');
+            }
+        };
+        $scope.validateID = function () {
+            console.log($scope.user.id);
+            if ($scope.user.id != null) {
+                $scope.iderrormessage = ''
+                console.log('not show');
+                return false;
+            }
+            else {
+                $scope.iderrormessage = 'not a valid input';
+                console.info('show');
+                return true;
+            }
+        }
+        $scope.gotoRegistration = function () {
+            $state.go('register');
+        };
     }])
 
     //homeCtrl provides the logic for the home screen
@@ -39,7 +123,7 @@
                 $scope.retailers = res.data;
 
             }, function () {
-                console.log("failed");
+                console.warn("search post failed");
             });
         };
 
@@ -49,17 +133,118 @@
         };
 
         $scope.setAsDefaultRetailer = function () {
-            console.log($customlocalstorage.getObject("defaulRetailer"));
+            console.log($customlocalstorage.getObject("defaultRetailer"));
 
             angular.forEach($scope.retailers, function (value, index) {
-                if(value.id == $scope.data.choice)
-                {
-                    $customlocalstorage.setObject("defaulRetailer",value);
+                if (value.id == $scope.data.choice) {
+                    $customlocalstorage.setObject("defaultRetailer", value);
+                    $state.go('products');
                 }
             });
 
             console.log("set default retailer.");
         }
+    }])
+
+    .controller("productsCtrl", ["$scope", "$state", "$customlocalstorage", "$http", "$ionicPopover", function ($scope, $state, $customlocalstorage, $http, $ionicPopover) {
+        $scope.data = { searchkey: '' };
+        $scope.data.choice = '';
+        $scope.retailers = null;
+
+        $scope.logout = function () {
+            localStorage.clear()
+            console.log('logout');
+            $state.go('register');
+        };
+
+        $ionicPopover.fromTemplateUrl('my-popover.html', {
+            scope: $scope
+        }).then(function (popover) {
+            $scope.popover = popover;
+        });
+
+        $scope.openPopover = function ($event) {
+            $scope.popover.show($event);
+        };
+        $scope.closePopover = function () {
+            $scope.popover.hide();
+        };
+        //Cleanup the popover when we're done with it!
+        $scope.$on('$destroy', function () {
+            $scope.popover.remove();
+        });
+        // Execute action on hide popover
+        $scope.$on('popover.hidden', function () {
+            // Execute action
+        });
+        // Execute action on remove popover
+        $scope.$on('popover.removed', function () {
+            // Execute action
+        });
+
+        $scope.refresh = function () {
+            //refresh binding
+            $scope.$broadcast("scroll.refreshComplete");
+        };
+
+        $scope.search = function () {
+
+            console.log($scope.data.searchkey)
+            var keyObj = {
+                Key: $scope.data.searchkey,
+                device: device.uuid
+            };
+
+            var req = {
+                method: 'POST',
+                url: 'http://localhost:36485/api/parties/key',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: JSON.stringify(keyObj)
+            }
+
+            $http(req).then(function (res) {
+                console.log(res.data);
+                $scope.retailers = res.data;
+
+            }, function () {
+                console.warn("search post failed");
+            });
+        };
+
+        $scope.searchSuggestion = function () {
+
+            nconsole.log("suggestion called");
+        };
+
+        $scope.setAsDefaultRetailer = function () {
+            console.log($customlocalstorage.getObject("defaultRetailer"));
+
+            angular.forEach($scope.retailers, function (value, index) {
+                if (value.id == $scope.data.choice) {
+                    $customlocalstorage.setObject("defaultRetailer", value);
+                }
+            });
+
+            console.log("set default retailer.");
+        }
+    }])
+
+    .controller("profileCtrl", ["$scope", "$state", "$customlocalstorage", "$http", function ($scope, $state, $customlocalstorage, $http) {
+        console.log('profileCtrl');
+    }])
+
+    .controller("ordersCtrl", ["$scope", "$state", "$customlocalstorage", "$http", function ($scope, $state, $customlocalstorage, $http) {
+        console.log('ordersCtrl');
+    }])
+
+    .controller("settingsCtrl", ["$scope", "$state", "$customlocalstorage", "$http", function ($scope, $state, $customlocalstorage, $http) {
+        console.log('settingsCtrl');
+    }])
+
+    .controller("feedbackCtrl", ["$scope", "$state", "$customlocalstorage", "$http", function ($scope, $state, $customlocalstorage, $http) {
+        console.log('feedbackCtrl');
     }])
 
     //errorCtrl managed the display of error messages bubbled up from other controllers, directives, myappService
