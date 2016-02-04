998afc6d7d87632aebd1c8931238c1fb019e5351
Venu Prasad H S <venuhs4@gmail.com>
2016-02-04 20:57:27 +0530

    added all files

---
+++ SmartRetailer2/platforms/android/assets/www/app/services.js
@@ -0,0 +1,142 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("myapp.services", []).factory("myappService", ["$rootScope", "$http", function ($rootScope, $http) {
+        var myappService = {};
+
+        //starts and stops the application waiting indicator
+        myappService.wait = function (show) {
+            if (show)
+                $(".spinner").show();
+            else
+                $(".spinner").hide();
+        };
+
+        return myappService;
+    }]);
+
+    angular.module('myapp.utils', []).factory('$customlocalstorage', ['$window', function ($window) {
+
+        var $customlocalstorage = {
+            set: function (key, value) {
+                $window.localStorage[key] = value;
+            },
+            get: function (key, defaultValue) {
+                return $window.localStorage[key] || defaultValue;
+            },
+            setObject: function (key, value) {
+                $window.localStorage[key] = JSON.stringify(value);
+            },
+            getObject: function (key) {
+                return JSON.parse($window.localStorage[key] || '{}');
+            },
+            getObjectorDefault:function(key,defaultValue)
+            {
+                return JSON.parse($window.localStorage[key] || defaultValue);
+            }
+        };
+
+        return $customlocalstorage;
+    }]).factory('$productlist', ['$http', function ($http) {
+        var products = null;
+        console.log("get products");
+        $http.get("http://192.168.1.55:8080/product").success(function (res) {
+            products = res;
+            console.log(products);
+            console.log("get products success");
+        }).then(function () {
+        });
+
+        return {
+            getProducts: function () {
+                return products;
+            },
+        };
+    }]).factory('$categoryTree', ['$http', function ($http) {
+        var category = [];
+        $http({
+            method: "GET",
+            url: "http://192.168.1.55:8080/category",
+        }).then(function (res) {
+            category = res.data;
+            angular.forEach(category, function (value, index) {
+                $http({
+                    method: "GET",
+                    url: "http://192.168.1.55:8080/category/segment/" + value.id
+                }).then(function (segmentsRes) {
+                    category[index].segments = segmentsRes.data;
+                    angular.forEach(category[index].segments, function (value2, index2) {
+                        $http({
+                            method: "GET",
+                            url: "http://192.168.1.55:8080/category/segment/subsegment/" + value2.id
+                        }).then(function (subsegmentsRes) {
+                            category[index].segments[index2].subsegment = subsegmentsRes.data;
+                        });
+                    });
+                });
+            });
+        }, function () {
+        });
+        return {
+            getTree: function () {
+                return category;
+            },
+        };
+    }])
+    .factory('$popupService', ['$ionicPopup', '$timeout', '$http', function ($ionicPopup, $timeout, $http) {
+        return {
+            showConfirm: function (title, template, data) {
+
+                var confirm = $ionicPopup.show({
+                    template: template,
+                    title: title,
+                    buttons: [{
+                        text: 'No',
+                        type: 'button-assertive',
+                        onTap: function (e) {
+                            data.confirm = false;
+                            console.log(false);
+                        }
+                    }, {
+                        text: '<b>Yes</b>',
+                        type: 'button-positive',
+                        onTap: function (e) {
+                            data.confirm = true;
+                            console.log(true);
+                        }
+                    }
+                    ]
+                });
+                return confirm;
+            },
+            showAlert: function (title, template) {
+                var alertPopup = $ionicPopup.alert({
+                    title: title,
+                    template: template
+                });
+                return alertPopup;
+            }
+        };
+    }])
+    .factory('$stringResource', ["$http", function ($http) {
+        var allstrings = [];
+        $http.get("/data/string-resource.json").then(function (res) {
+            allstrings = res.data;
+        }, function (err) {
+            console.log(err);
+        });
+
+        return {
+            getValue: function (k) {
+                var finValue = '';
+                angular.forEach(allstrings, function (val, index) {
+                    if (val.Key === k) {
+                        console.log(val.Value);
+                        finValue = val.Value;
+                    }
+                });
+                return finValue;
+            }
+        }
+    }]);
+})();
---
+++ SmartRetailer2/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,33 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/pushbots-cordova-plugin/www/pushbots.js",
+        "id": "pushbots-cordova-plugin.PushbotsPlugin",
+        "clobbers": [
+            "PushbotsPlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-geolocation": "1.0.1",
+    "cordova-plugin-device": "1.1.0",
+    "pushbots-cordova-plugin": "1.3.0",
+    "cordova-plugin-android-support-v4": "21.0.1"
+}
+// BOTTOM OF METADATA
+});
